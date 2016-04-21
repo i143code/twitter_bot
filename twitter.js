120f2f3be676474a09b2b37f7a0e2d23d6e74ee6
ashish ranjan <ranjanah143code@yahoo.com>
2016-04-21 02:21:29 -0400

    Update twitter.js

--- twitter.js
+++ twitter.js
@@ -1,7 +1,7 @@
 module.exports  ={
-  consumer_key:         'azy4GYCXD0iUavi7Uf71uVqzD',
-  consumer_secret:      'bvIAUAxW0rfmki7pFyV1l7mOol7qN5H7sMcDEo1xkJiMoYnFvL',
-  access_token:         '722966957700485120-157q4UaM51SSRHlthkwbvt0E33lFPGm',
-  access_token_secret:  'Uf7gmLjfwVM3mbZZWpjvbtmoWQUpmyFYEmKY4d6ly8IFf',
+  consumer_key:         '',
+  consumer_secret:      '',
+  access_token:         '',
+  access_token_secret:  '',
   timeout_ms:           60*1000,  // optional HTTP request timeout to apply to all requests.
 }
