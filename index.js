f6eb3cacca35fd14e83be3ea5b193989ff0cb181
i143code <ranjanah143code@yahoo.com>
2016-04-20 22:56:04 -0400

    added the set interval method

--- index.js
+++ index.js
@@ -1,15 +1,53 @@
 var Twit = require('twit')
 console.log("runnign server");
+//twitter config
+var twitter = require('./twitter');
+
+var T = new Twit(twitter);
+
+var tweet = { status: "Hello , I am a bot!!" };
+
+// a simple post request
+T.post('statuses/update',tweet, gotData );
+
+//a function
+function gotData(err, data, response){
+	
+       console.log(data);
+}
+
+// two tweet with rainbow keywords
+var tweet1 = { q: 'rainbow', count: 1 };
 
 
-var T = new Twit({
-  consumer_key:         'azy4GYCXD0iUavi7Uf71uVqzD',
-  consumer_secret:      'bvIAUAxW0rfmki7pFyV1l7mOol7qN5H7sMcDEo1xkJiMoYnFvL',
-  access_token:         '722966957700485120-157q4UaM51SSRHlthkwbvt0E33lFPGm',
-  access_token_secret:  'Uf7gmLjfwVM3mbZZWpjvbtmoWQUpmyFYEmKY4d6ly8IFf',
-  timeout_ms:           60*1000,  // optional HTTP request timeout to apply to all requests.
-})
-// hi i am ashish bot 
-T.post('statuses/update', { status: 'Hi, I am learning how to make a bot using twiiter apis and my server is tweeting this :)' }, function(err, data, response) {
-  console.log(data)
-})
+//make a simple get request
+// T.get('search/tweets',tweet1, gotData);
+
+
+//call the function
+tweetit();
+
+//setinterval
+
+setInterval(tweetit, 1000*10);
+
+//function tweet it in interval
+
+function tweetit(){
+//create a random number 
+var r = Math.floor(Math.random()*99)
+var tweet = { status: "Hello , I am a bot!!" + r +"i am posting status for testing!!" };
+
+// a simple post request
+T.post('statuses/update',tweet, gotData );
+
+//a function
+function gotData(err, data, response){
+       // console.log(data);
+       if(err){
+       	console.log(err);
+       }
+
+       console.log("It worked!!");
+    }
+}
